9a50866e0b37aac0c8f07a1b6b338ad54bc624f3
finn-01 <nguyensam576@gmail.com>
2022-01-21 00:20:54 +0700

    Heading

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 import Navbar from "./components/Navbar";
+import Heading from "./components/Heading";
 
 const App = () => {
 	return (
 		<div className="container-md">
 			<Navbar />
+			<Heading />
 		</div>
 	);
 };
---
+++ src/components/Heading.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Heading = () => {
+	return (
+		<div>
+			<img src="../images/main-banner.png" className="main-banner" />
+		</div>
+	);
+};
+
+export default Heading;
